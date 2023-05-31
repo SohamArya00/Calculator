3c4c50d82df2a9c1ece4eeb7f2ed905cb55d1b80
SohamArya00 <101948981+SohamArya00@users.noreply.github.com>
2023-05-31 23:33:55 +1200

    Create App.java

---
+++ src/main/java/com/soham/calculator/App.java
@@ -0,0 +1,13 @@
+package com.soham.calculator;
+
+
+import com.soham.calculator.ui.CalculatorUI;
+
+
+public class App {
+  
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
+}
