165a822cca33c93baa8dafe52fa99878e6c02d08
SohamArya00 <101948981+SohamArya00@users.noreply.github.com>
2023-05-31 23:34:16 +1200

    Update App.java

--- src/main/java/com/soham/calculator/App.java
+++ src/main/java/com/soham/calculator/App.java
@@ -1,11 +1,8 @@
 package com.soham.calculator;
 
-
 import com.soham.calculator.ui.CalculatorUI;
 
-
 public class App {
-  
 
     public static void main(String[] args) {
         new CalculatorUI();
